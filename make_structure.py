41ffeabebae57b9b20f605ef62ef106656862252
gschare <grschare1315@gmail.com>
2021-04-26 16:57:21 -0700

    Work on finding missing spring 2017 DH links

--- make_structure.py
+++ make_structure.py
@@ -11,17 +11,18 @@ import csv
 from bs4 import BeautifulSoup
 
 def main():
+    global CORRECTIONS
     with open(CORRECTIONS_FILE, 'r') as fp:
         reader = csv.reader(fp)
         CORRECTIONS = {row[0]:row[1] for row in reader}
     mapping = {}
     missing = []
-    for semester in [sp17]:
+    for semester in [sp17dh]:
         temp_mapping, temp_missing = semester()
         mapping.update(temp_mapping)
         missing.extend(temp_missing)
     #print(mapping)
-    #print(missing)
+    [print(x) for x in missing]
     #print(CORRECTIONS)
 
     with open('mapping.csv', 'w') as fp:
@@ -72,6 +73,23 @@ def sanitize(string):
     table = string.maketrans(replacements)
     return string.translate(table).lower()
 
+def map_links(parent_file, parent_file_path, files, as_folders=False):
+    mapping = {}
+    missing = []
+    for title, filename in files:
+        if not os.path.exists(FOLDER_PREFIX + filename):
+            if filename in CORRECTIONS.keys():
+                filename = CORRECTIONS[filename]
+            else:
+                missing.append((parent_file, filename))
+                continue   # don't add to mapping
+
+        if as_folders:
+            mapping[filename] = f'{parent_file_path}/{title}/index.html'
+        else:
+            mapping[filename] = f'{parent_file_path}/{title}.html'
+    return mapping, missing
+
 def sp17():
     mapping = {}
     missing = []
@@ -90,35 +108,58 @@ def sp17():
     fieldnotes = get_all_links_from_file(FOLDER_PREFIX + fieldnotes_file)
     profiles = get_all_links_from_file(FOLDER_PREFIX + profiles_file)
 
-    for student, student_file in fieldnotes:
-        if not os.path.exists(FOLDER_PREFIX + student_file):
-            if student_file in CORRECTIONS.keys():
-                student_file = CORRECTIONS[student_file]
-            else:
-                missing.append((fieldnotes_file, student_file))
-                continue   # don't add to mapping
+    fieldnotes_mapping, fieldnotes_missing = map_links(fieldnotes_file, f'{semester_name}/{FIELDNOTES_FOLDER_NAME}', fieldnotes, as_folders=True)
+    profiles_mapping, profiles_missing = map_links(profiles_file, f'{semester_name}/{PROFILES_FOLDER_NAME}', profiles, as_folders=false)
 
-        mapping[student_file] = f'{semester_name}/{FIELDNOTES_FOLDER_NAME}/{student}/index.html'
-        pages = get_all_links_from_file(FOLDER_PREFIX + student_file)
-        for title, note_file in pages:
-            mapping[note_file] = f'{semester_name}/{FIELDNOTES_FOLDER_NAME}/{student}/{title}.html'
+    mapping.update(fieldnotes_mapping)
+    mapping.update(profiles_mapping)
+    missing.extend(fieldnotes_missing)
+    missing.extend(profiles_missing)
 
-    for student, student_file in profiles:
-        if not os.path.exists(FOLDER_PREFIX + student_file):
-            if student_file in CORRECTIONS.keys():
-                student_file = CORRECTIONS[student_file]
-            else:
-                missing.append((profiles_file, student_file))
-                continue   # don't add to mapping
+    for student_file, new_path in fieldnotes_mapping.items():
+        student_notes = get_all_links_from_file(FOLDER_PREFIX + student_file)
+        student_notes_mapping, student_notes_missing = map_links(student_file, new_path.rsplit('/index.html')[0], student_notes, as_folders=False)
 
-        mapping[student_file] = f'{semester_name}/{PROFILES_FOLDER_NAME}/{student}.html'
+        mapping.update(student_notes_mapping)
+        missing.extend(student_notes_missing)
 
     return mapping, missing
 
 def sp17dh():
+    mapping = {}
+    missing = []
     semester_name = 'sp17dh'
-    url = 'http://fieldnotes.makingandknowing.org/mainSpace/Spring%202017%20Digital%20Archive.html'
-    soup = BeautifulSoup(get_html(url), 'html.parser')
+
+    filename = unquote('Spring%202017%20Digital%20Archive.html')
+    mapping[filename] = f'{semester_name}/index.html'
+
+    soup = BeautifulSoup(get_html_from_file(FOLDER_PREFIX + filename), 'html.parser')
+
+    fieldnotes_file = CORRECTIONS[unquote(soup.find(string=re.compile('Field Notes')).parent['href'])]
+    mapping[fieldnotes_file] = f'{semester_name}/{FIELDNOTES_FOLDER_NAME}/index.html'
+
+    # Course page
+    course_link = soup.find(string=re.compile('HIST GR8975')).parent
+    course_title = sanitize(course_link.string)
+    course_file = unquote(course_link['href'])
+    mapping[course_file] = f'{semester_name}/{course_title}.html'
+
+    # field notes, including TA field notes and example DH
+    # TODO: confirm folder structure for TA field notes and example DH
+    fieldnotes = get_all_links_from_file(FOLDER_PREFIX + fieldnotes_file)
+    fieldnotes_mapping, fieldnotes_missing = map_links(fieldnotes_file, f'{semester_name}/{FIELDNOTES_FOLDER_NAME}', fieldnotes, as_folders=True)
+
+    mapping.update(fieldnotes_mapping)
+    missing.extend(fieldnotes_missing)
+
+    for student_file, new_path in fieldnotes_mapping.items():
+        student_notes = get_all_links_from_file(FOLDER_PREFIX + student_file)
+        student_notes_mapping, student_notes_missing = map_links(student_file, new_path.rsplit('/index.html')[0], student_notes, as_folders=False)
+
+        mapping.update(student_notes_mapping)
+        missing.extend(student_notes_missing)
+
+    return mapping, missing
 
 def fa16():
     url = 'http://fieldnotes.makingandknowing.org/mainSpace/Fall%202016%20Archive.html'
