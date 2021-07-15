0ec5f56e6b1b8af5ce06aeadc80a21d35bd88bf3
gschare <grschare1315@gmail.com>
2021-07-15 14:29:22 -0400

    index -> toc

--- make_listing.sh
+++ make_listing.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#ls -R1A /mnt/c/code/github/fieldnotes-content/content/ > index.txt
-find /mnt/c/code/github/fieldnotes-content/content/ > index.txt
+#ls -R1A /mnt/c/code/github/fieldnotes-content/content/ > toc.txt
+find /mnt/c/code/github/fieldnotes-content/content/ > toc.txt
