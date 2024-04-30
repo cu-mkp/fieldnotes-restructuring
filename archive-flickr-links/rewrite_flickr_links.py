e4bc23d6647bd515ee01a9be71d6f181c3bf876b
Gregory Schare <grschare1315@gmail.com>
2024-04-30 18:36:21 -0400

    another part of flickr script that doesn't do API calls

--- archive-flickr-links/rewrite_flickr_links.py
+++ archive-flickr-links/rewrite_flickr_links.py
@@ -87,10 +87,9 @@ def flickr_photoset_getInfo(photoset_id, user_id):
 def main(dry_run=True):
     fix_tags(dry_run=dry_run)
 
-def fix_tags(dry_run=True):
+def record_tag_flickr_data():
     # please run this file from the directory which contains all the HTML files
     # (possibly within subfolders) you want to modify.
-
     ROOT_DIR = os.getcwd()
 
     # a list of files to modify.
@@ -144,30 +143,10 @@ def fix_tags(dry_run=True):
                 r = requests.get(new_link)
                 assert r.status_code == 200, f'file {f}: could not locate object:\n  {s3_object_name}\nfrom flickr url:\n  {anchor["href"]}\nat s3 url:\n  {new_link}\nGot response code: {str(r.status_code)}.\nDebug request:\n  {data["request"]}'
 
-                # if it's a video, change it to a video. otherwise just change the src.
-                if data['media_type'] == 'video':
-                    img.name = 'video'
-                    del img['alt']
-                    del img['src']
-                    img['controls'] = None
-                    img['width'] = data['width']
-                    img['height'] = data['height']
-                    src = soup.new_tag('source')
-                    src['src'] = new_link
-                    img.append(src)
-                    img.append('Your browser does not support the video tag.')
-                    print(img)
-                else:
-                    # modify the img to use the AWS link
-                    img['src'] = new_link
-
-                del anchor['data-flicker-embed']
-
                 # look for the script tag associated with the anchor, and destroy it.
                 script = anchor.next_sibling
                 assert script.name == 'script'
                 assert script['src'] == '//embedr.flickr.com/assets/client-code.js'
-                script.decompose()
                 
                 # save the album id, if any.
                 #
@@ -208,11 +187,6 @@ def fix_tags(dry_run=True):
                 exceptions.append({'file': f, 'imgs': list(map(str, imgs))})
             print("img tags with flickr links:", len(imgs))
 
-        # write the file
-        if not dry_run:
-            with open(f, 'wb') as fo:
-                fo.write(soup.prettify('utf-8'))
-
     # debug info
     debug_folder = '../../fieldnotes-restructuring/archive-flickr-links'
 
@@ -235,6 +209,76 @@ def fix_tags(dry_run=True):
     albums_debug_file = os.path.join(debug_folder, 'albums.json')
     with open(albums_debug_file, 'w') as f:
         json.dump(albums, f, ensure_ascii=False, indent=4)
+
+def fix_tags(dry_run=True):
+    # please run this file from the directory which contains all the HTML files
+    # (possibly within subfolders) you want to modify.
+
+    ROOT_DIR = os.getcwd()
+
+    # a list of files to modify.
+    # in theory you could derive this by grepping for flickr links.
+    # in practice to be safe I'm just going to check every HTML file in the repo.
+    plan = list_html_files(ROOT_DIR)
+
+    # json info
+    json_folder = '../../fieldnotes-restructuring/archive-flickr-links'
+
+    # write rewrite log
+    rewrites_json_file = os.path.join(json_folder, 'rewrites.json')
+    with open(rewrites_json_file, 'r') as f:
+        rewrites = json.load(f)
+
+    for f in plan:
+        with open(f, 'r') as fo:
+            soup = BeautifulSoup(fo, 'html.parser')
+
+            # look for flickr anchors that have the special embedding method
+            anchors_with_embed = soup.find_all('a', href=re.compile(r'flickr.com/photos/[^/]+/\d+/'), attrs={'data-flickr-embed': 'true'})
+            for anchor in anchors_with_embed:
+                # look for the img tag associated with the anchor
+                img = anchor.img
+                assert img['alt'] == anchor['title']
+
+                album_id = match.group(1) if (match := re.search('/in/album-([^/]+)', anchor['href'])) else None
+                user_id = re.search('flickr.com/photos/([^/]+)', anchor['href']).group(1)
+                photo_id = re.search('flickr.com/photos/[^/]+/([^/]+)', anchor['href']).group(1)
+
+                data = rewrites[f].get(photo_id)
+                if data == None:
+                    continue
+
+                s3_url = data['s3_url']
+
+                # if it's a video, change it to a video. otherwise just change the src.
+                if data['media_type'] == 'video':
+                    img.name = 'video'
+                    del img['alt']
+                    del img['src']
+                    img['controls'] = None
+                    #img['width'] = data['width']
+                    #img['height'] = data['height']
+                    src = soup.new_tag('source')
+                    src['src'] = s3_url
+                    img.append(src)
+                    img.append('Your browser does not support the video tag.')
+                    print(img)
+                else:
+                    # modify the img to use the AWS link
+                    img['src'] = s3_url
+
+                del anchor['data-flicker-embed']
+
+                # look for the script tag associated with the anchor, and destroy it.
+                script = anchor.next_sibling
+                assert script.name == 'script'
+                assert script['src'] == '//embedr.flickr.com/assets/client-code.js'
+                script.decompose()
+
+        # write the file
+        if not dry_run:
+            with open(f, 'wb') as fo:
+                fo.write(soup.prettify('utf-8'))
 
 if __name__ == "__main__":
 
